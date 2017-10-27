dff6e54e61a65e2597f3d882ddd760e07d3fc0bc
Erin <erindon@umich.edu>
2017-10-27 12:18:00 -0400

    fucky implementation you are going too fast

--- rpn.py
+++ rpn.py
@@ -1,8 +1,17 @@
 #!/usr/bin/env python3
 
 def calculate(myarg):
-    pass
-
+	stack = list()
+	for token in myarg.split():
+		if token =='+':
+			arg1 = stack.pop()
+			arg2 = stack.pop()
+			result = arg1+arg2
+			stack.append(result)
+		else:
+			stack.append(token)
+	print(stack)
+	return stack.pop()
 def main():
     while True:
         calculate(input("rpn calc> "))
