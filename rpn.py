98e9ff9788e23674900ca29c4ebd64e4878a9c7c
Erin <erindon@umich.edu>
2017-10-27 12:34:25 -0400

    made some changed

--- rpn.py
+++ rpn.py
@@ -1,21 +1,29 @@
 #!/usr/bin/env python3
 
+import operator
+
+def subtract (a,b):
+        return a - b
+def add (a,b):
+        return a + b
+ops = {
+        '+':operator.add,
+        '-':operator.sub,
+        '*':operator.mul
+}
+
 def calculate(myarg):
 	stack = list()
 	for token in myarg.split():
-		if token =='+':
-			arg1 = stack.pop()
-			arg2 = stack.pop()
-			result = arg1+arg2
-			stack.append(result)
-		elif token == '-':
-			arg1 = stack.pop()
-			arg2 = stack.pop()
-			result = arg2 - arg1
-			stack.append(result)
-		else:
-			stack.append(int(token))
-	print(stack)
+	        try:
+                        stack.append(int(token))
+                except ValueError:
+                        arg1 = stack.pop()
+                        arg2 = stack.pop()
+                        function = ops[token]
+                        result =  function(arg1, arg2)
+                        stack.append(result)
+#	print(stack)
 	return stack.pop()
 def main():
     while True:
